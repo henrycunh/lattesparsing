527a8ee67c0ad18c7e5eed7f982c936511fb9e8c
Nayra Kaline <ruheallein@gmail.com>
2017-04-05 16:15:40 -0300

    Erro em Patente corrigido

--- ics/patente.php
+++ ics/patente.php
@@ -39,7 +39,7 @@
         //Caso possua patentes
         if(isset($data['PRODUCAO-TECNICA']['PATENTE'])):
           $patentesRaw = $data['PRODUCAO-TECNICA']['PATENTE'];
-
+        if(array_keys($patentesRaw)[0] === '@attributes') $patentesRaw = array($patentesRaw);
         //Percorrer lista de patentes
         foreach ($patentesRaw as $patente) {
           //Classe temporária para atribuir valores
